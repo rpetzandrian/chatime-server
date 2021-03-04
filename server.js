8739ea52656129cb2578eb388f44dba74b2e0734
Rpetz13 <rpetzandrian@gmail.com>
2021-03-04 08:35:36 +0700

    add new env

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 8000;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json([{ message: "Ini get" }]);
-});
-
 const router = require("./routes");
 router(app, "/api/v1");
 
+app.get("/", (req, res) => {
+  res.send({
+    message: "Home",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send({
+    message: "Page not found",
+    statuscode: 404,
+  });
+});
+
 app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
+  console.log(`Server started on http://${process.env.HOST}:${port}`);
 });
